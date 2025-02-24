e9110a4b11e3b4e88faed903b8bff6c97aabcc56
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-02-24 23:33:06 +0700

    Update run.sh

--- EarnOS/run.sh
+++ EarnOS/run.sh
@@ -37,7 +37,8 @@ create_default_configs() {
     "delayEachAccount": [5, 8],
     "timeToRestartAllAccounts": 300,
     "howManyAccountsRunInOneTime": 10,
-    "doTasks": true
+    "doTasks": true,
+    "skipWatchVideoTask":true,
 }
 EOL
 }
